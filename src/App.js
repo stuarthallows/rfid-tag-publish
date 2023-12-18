6d0f54b856bb4822a97dbe21afaecfbc5897b968
Stuart Hallows <stuart.hallows@gmail.com>
2023-12-18 21:38:24 +1030

    Client calls API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
-  const value = 'World';
+  const [value, setValue] = useState('');
+
+  useEffect(() => {
+    fetch('api/TagScanned?name=Stuart')
+      .then(response => response.text())
+      .then(data => setValue(data))
+      .catch(error => console.error('Error fetching data from api/TagScanned', error));
+  }, []);
+
   return <div>Hello {value}</div>;
 }
 
