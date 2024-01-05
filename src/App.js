d9f5000cff303ce29b32dae36f2f765ca7bbb555
Stuart Hallows <stuart.hallows@gmail.com>
2024-01-06 08:40:51 +1030

    Use Vite

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { useSignalRClient } from './useSignalRClient';
-
-export function App() {
-  var tagId = useSignalRClient();
-
-  return <div> Tag Id: {tagId}</div>;
-}
